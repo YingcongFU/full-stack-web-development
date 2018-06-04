c84c2724860470fc1d68959d139ddaa1d94d67aa
June Fu <fyc6401@163.com>
2018-06-04 11:44:00 -0700

    add draw

---
+++ Desktop/full-stack/draw.py
@@ -0,0 +1,19 @@
+import turtle
+
+def draw_square():
+    window=turtle.Screen()
+    window.bgcolor("white")
+    brad=turtle.Turtle()
+    turtle.color('yellow')
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+    window.exitonclick
+
+draw_square()
