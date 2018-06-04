f6e28b9cdbe6aec113871c053d515d4e7bfbfd38
June Fu <fyc6401@163.com>
2018-06-04 16:37:32 -0700

    modify draw

--- Desktop/full-stack/draw.py
+++ Desktop/full-stack/draw.py
@@ -1,19 +1,33 @@
 import turtle
 
-def draw_square():
-    window=turtle.Screen()
-    window.bgcolor("white")
-    brad=turtle.Turtle()
-    turtle.color('yellow')
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+def draw_square(square):
+    for i in range(1,5):
+        square.forward(100)
+        square.right(90)
 
+def draw_art():
+    window=turtle.Screen()
+    window.bgcolor("black")
+    brad=turtle.Turtle()
+    brad.shape('turtle')
+    brad.color('yellow')
+    brad.speed(2)
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+
+    angie=turtle.Turtle()
+    angie.shape('arrow')
+    angie.color('blue')
+    angie.circle(100)
+
+    shabi=turtle.Turtle()
+    shabi.shape('turtle')
+    shabi.color('red')
+    for j in range(1,4):
+        shabi.forward(100)
+        shabi.right(120)
+    
     window.exitonclick
 
-draw_square()
+draw_art()
