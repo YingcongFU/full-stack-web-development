4333708c0756e261dabf0e361e4fcba04590900a
June Fu <fyc6401@163.com>
2018-06-04 11:43:06 -0700

    add rename

---
+++ Desktop/full-stack/rename.py
@@ -0,0 +1,11 @@
+import os
+def rename_files():
+    file_list=os.listdir(r"/Users/june/Desktop/prank")
+    saved_path=os.getcwd()
+    print("Current Working Directory is "+saved_path)
+    os.chdir("/Users/june/Desktop/prank")
+    print (file_list)
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None,"0123456789"))
+rename_files()
+
